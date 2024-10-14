4a0e9e9af188ab91af094ff0d5b15a86cb6aff3f
Thong2705 <thongnltt2705@gmail.com.com>
2024-10-14 13:26:24 +0700

    Add project files.

---
+++ CourseProject/Controllers/CoursesController.cs
@@ -0,0 +1,33 @@
+﻿using CourseProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourseProject.Controllers
+{
+    public class CoursesController: Controller
+    { 
+        VNR_InternShipEntities _db = new VNR_InternShipEntities();
+
+        public ActionResult Index()
+        {
+            var courses = _db.KhoaHocs.ToList();
+            return View(courses);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var khoaHoc = _db.KhoaHocs.Find(id);
+            if (khoaHoc == null)
+            {
+                return HttpNotFound();
+            }
+
+            var monHocList = _db.MonHocs.Where(mh => mh.KhoaHocID == id).ToList();
+            ViewBag.KhoaHoc = khoaHoc.TenKhoaHoc;
+            return View(monHocList);
+        }
+    }
+}
